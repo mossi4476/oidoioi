ac8fa35b7c81d9683785a3cf6898b28ed8cf2953
mossi4476 <mossid99@ptd.edu.vn>
2021-09-27 16:23:05 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  introTitle: "HELLO",
+  introDesc: `Hi hê lô mô tô chào em`,
+  btnIntro: "hihi-haha",
+  title: "Thương Thương 😙",
+  desc: "I Love You ",
+  btnYes: " Thích anh rồi đúng không ? <33",
+  btnNo: "Không, Anh trai à :3",
+  question: "Thế giới có hơn 7 tỉ người mà sao em lại thích anh <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều  😘😘",
+  messDesc: "Tối anh chở đi chơi nhé.",
+  btnAccept: "Tới công chuyện luôn  <3",
+  messLink: "https://m.me/Nguyentiendungtpm6dtu",
+};
